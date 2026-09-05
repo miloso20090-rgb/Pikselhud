064ce48f2b914dab3f340ba5e9cd7e5c00d2acf9
miloso20090-rgb <miloso20090@gmail.com>
2026-09-05 22:20:39 +0200

    Put keybind in PixelHub category

--- src/main/java/com/pikselhud/PikselHudClient.java
+++ src/main/java/com/pikselhud/PikselHudClient.java
@@ -3,10 +3,13 @@ package com.pikselhud;
 import com.pikselhud.config.ConfigManager;
 import com.pikselhud.gui.MainScreen;
 import com.pikselhud.hud.HudRenderer;
+import com.pikselhud.nameprotect.NameProtectManager;
+import com.pikselhud.nameprotect.FriendsOutlineRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -19,14 +22,21 @@ public class PikselHudClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ConfigManager.load();
+        NameProtectManager.syncFromConfig(ConfigManager.data);
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.pikselhud.open_gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_P, KeyBinding.Category.MISC));
+                "key.pikselhud.open_gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_P,
+                "category.pixelhub"
+        ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openGuiKey.wasPressed()) {
                 if (client.currentScreen == null) client.setScreen(new MainScreen(null));
             }
         });
-        HudElementRegistry.addLast(Identifier.of(MOD_ID, "hud"),
-                (drawContext, tickCounter) -> HudRenderer.render(drawContext));
+
+        HudElementRegistry.addLast(Identifier.of(MOD_ID, "hud"), (drawContext, tickCounter) -> HudRenderer.render(drawContext));
+        WorldRenderEvents.AFTER_ENTITIES.register(FriendsOutlineRenderer::render);
     }
 }
